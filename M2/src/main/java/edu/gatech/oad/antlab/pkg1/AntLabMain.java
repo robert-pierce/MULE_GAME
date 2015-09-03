7739b63da78440dc99e41a81368b4b242da27653
kharris48 <kharris48@gatech.edu>
2015-09-03 13:52:33 -0400

    Revert "I used the wrong GTID in my previous commit - Kamryn"
    This reverts commit 81f2240cc733ff06cca8051c06361a11c99ef98d.

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -74,7 +74,7 @@ import edu.gatech.oad.antlab.pkg2.*;
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
                   Person5 p5 = new Person5("Kamryn Harris");
-                  toPrint += p5.toString("kharris48");
+                  toPrint += p5.toString("902968981");
 		  
         System.out.println(toPrint);
         
