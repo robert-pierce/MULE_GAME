907b21640106e12edb839263a3f6c1eec0b0c44e
Robert Pierce <rpierce31@gatech.edu>
2015-09-04 14:29:41 -0400

    deleted M2/src/main directory, add M2/src directory -- Robert Pierce

---
+++ M2/src/edu/gatech/oad/antlab/person/Person2.java
@@ -0,0 +1,66 @@
+package edu.gatech.oad.antlab.person;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+* By Benjamin Bierbaum
+**/
+
+/**
+ *  A simple class for person 2
+ *  returns their name and a
+ *  modified string 
+ *
+ * @author Bob
+ * @version 1.1
+ */
+public class Person2 {
+    /** Holds the persons real name */
+    private String name;
+	 	/**
+	 * The constructor, takes in the persons
+	 * name
+	 * @param pname the person's real name
+	 */
+	 public Person2(String pname) {
+	   name = pname;
+	 }
+	/**
+	 * This method should take the string
+	 * input and return its characters in
+	 * random order.
+	 * given "gtg123b" it should return
+	 * something like "g3tb1g2".
+	 *
+	 * @param input the string to be modified
+	 * @return the modified string
+	 */
+	private String calc(String input) {
+		Random rand = new Random();
+		List<Character> charList = new ArrayList<Character>();
+
+        for(char c : input.toCharArray()){
+            charList.add(c);
+        }
+
+        StringBuilder out = new StringBuilder(input.length());
+
+        while(charList.size() != 0) {
+            int randInt = (int)(Math.random() * charList.size());
+            out.append(charList.remove(randInt));
+        }
+        return (out.toString());
+	}
+	/**
+	 * Return a string rep of this object
+	 * that varies with an input string
+	 *
+	 * @param input the varying string
+	 * @return the string representing the 
+	 *         object
+	 */
+	public String toString(String input) {
+	  return name + calc(input);
+	}
+}
