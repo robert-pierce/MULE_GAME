52e2f4124b37e337fc2686a9e43feff5725250d6
homeSickFungus <rpierce3@gmail.com>
2015-10-15 19:16:27 -0400

    implmenting buying and selling from the store. As of now, sell food is fully implemented. There are dynamic labels though. Next, implement sell and buy for energy, smithore, and crystite. -Robert

--- MULEgame/src/application/Store.java
+++ MULEgame/src/application/Store.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Deque;
 import java.util.List;
 import application.GameRunner.Difficulty;
+import javafx.beans.property.SimpleIntegerProperty;
 
 /**
  * Created by KillDogg on 10/8/2015.
@@ -13,30 +14,42 @@ public class Store {
     private GameRunner.Difficulty difficulty;
     private Deque<Mule> muleDeque;
     private int initialMuleStock;
-    private int foodStock;
-    private int energyStock;
-    private int smithoreStock;
-    private int crystiteStock;
-    private int mulePrice;
-    private int foodPrice;
-    private int energyPrice;
-    private int smithorePrice;
-    private int crystitePrice;
+    //private int foodStock;
+    //private int energyStock;
+    //private int smithoreStock;
+    //private int crystiteStock;
+    //private int mulePrice;
+    //private int foodPrice;
+    //private int energyPrice;
+    //private int smithorePrice;
+    //private int crystitePrice;
     private int foodEquipPrice;
     private int energyEquipPrice;
     private int smithoreEquipPrice;
     private int crystiteEquipPrice;
 
+    private SimpleIntegerProperty muleStockProperty;
+    private SimpleIntegerProperty foodStockProperty;
+    private SimpleIntegerProperty energyStockProperty;
+    private SimpleIntegerProperty smithoreStockProperty;
+    private SimpleIntegerProperty crystiteStockProperty;
+    
+    private SimpleIntegerProperty mulePriceProperty;
+    private SimpleIntegerProperty foodPriceProperty;
+    private SimpleIntegerProperty energyPriceProperty;
+    private SimpleIntegerProperty smithorePriceProperty;
+    private SimpleIntegerProperty crystitePriceProperty;
+    
     public Store(Difficulty difficulty) {
         this.difficulty = difficulty;
         System.out.println("The store thinks difficulty is:" + difficulty);
         muleDeque = new ArrayDeque<Mule>();
         fillStore();
-        this.mulePrice = 100;
-        this.foodPrice = 30;
-        this.energyPrice = 25;
-        this.smithorePrice = 50;
-        this.crystitePrice = 100;
+        mulePriceProperty = new SimpleIntegerProperty(100);
+        foodPriceProperty = new SimpleIntegerProperty(30);
+        energyPriceProperty = new SimpleIntegerProperty(25);
+        smithorePriceProperty = new SimpleIntegerProperty(50);
+        crystitePriceProperty = new SimpleIntegerProperty(100);
         this.foodEquipPrice = 25;
         this.energyEquipPrice = 50;
         this.smithoreEquipPrice = 75;
@@ -46,16 +59,18 @@ public class Store {
     private void fillStore() {
         if(difficulty.equals(Difficulty.BEGINNER)) {
         	initialMuleStock = 25;
-        	foodStock = 16;
-        	energyStock = 16;
-        	smithoreStock = 0;
-        	crystiteStock = 0;
+        	muleStockProperty = new SimpleIntegerProperty(25);
+        	foodStockProperty = new SimpleIntegerProperty(16);
+        	energyStockProperty = new SimpleIntegerProperty(16);
+        	smithoreStockProperty = new SimpleIntegerProperty(0);
+        	crystiteStockProperty = new SimpleIntegerProperty(0);
         } else {
         	initialMuleStock = 14;
-            foodStock = 8;
-            energyStock = 8;
-            smithoreStock = 8;
-            crystiteStock = 0;
+        	muleStockProperty = new SimpleIntegerProperty(14);
+            foodStockProperty = new SimpleIntegerProperty(8);
+            energyStockProperty = new SimpleIntegerProperty(8);
+            smithoreStockProperty = new SimpleIntegerProperty(8);
+            crystiteStockProperty = new SimpleIntegerProperty(0);
         }
         createMules();
     }
@@ -69,6 +84,7 @@ public class Store {
 
     public Mule sellMule() {
     	if (getMuleStock() > 0) {
+    		muleStockProperty.setValue(muleStockProperty.subtract(1).getValue());
 	        return muleDeque.poll();
     	} else {
     		return null;
@@ -79,9 +95,50 @@ public class Store {
         System.out.println("The muleStock is: " + muleDeque.size());
         return muleDeque.size();
     }
+    
+    public SimpleIntegerProperty getMuleStockProperty() {
+    	return muleStockProperty;
+    }
+    
+    public int getFoodStock() {
+        return foodStockProperty.get();
+    }
+    
+    public SimpleIntegerProperty getFoodStockProperty() {
+    	return foodStockProperty;
+    }
+    
+    public int getEnergyStock() {
+        return energyStockProperty.get();
+    }
+    
+    public SimpleIntegerProperty getEnergyStockProperty() {
+    	return energyStockProperty;
+    }
+    
+    public int getSmithoreStock() {
+        return smithoreStockProperty.get();
+    }
+    
+    public SimpleIntegerProperty getSmithoreStockProperty() {
+    	return smithoreStockProperty;
+    }
+    
+    public int getCrystiteStock() {
+        return crystiteStockProperty.get();
+    }
+    
+    public SimpleIntegerProperty getCrystiteStockProperty() {
+    	return crystiteStockProperty;
+    }
+    
 
     public int getMulePrice() {
-        return mulePrice;
+        return mulePriceProperty.get();
+    }
+    
+    public SimpleIntegerProperty getMulePriceProperty() {
+        return mulePriceProperty;
     }
     
     public int getFoodEquipPrice() {
@@ -101,20 +158,49 @@ public class Store {
     }
     
     public int getFoodPrice() {
-    	return foodPrice;
+    	return foodPriceProperty.get();
+    }
+    
+    public SimpleIntegerProperty getFoodPriceProperty() {
+        return foodPriceProperty;
     }
     
     public void incrementFood() {
-    	foodStock++;
+    	foodStockProperty.setValue(foodStockProperty.add(1).getValue());
     }
+    
+    public int getEnergyPrice() {
+    	return energyPriceProperty.get();
+    }
+    
+    public SimpleIntegerProperty getEnergyPriceProperty() {
+        return energyPriceProperty;
+    }
+    
+    public int getSmitorePrice() {
+    	return smithorePriceProperty.get();
+    }
+    
+    public SimpleIntegerProperty getSmithorePriceProperty() {
+        return smithorePriceProperty;
+    }
+    
+    public int getCrystitePrice() {
+    	return crystitePriceProperty.get();
+    }
+    
+    public SimpleIntegerProperty getCrystitePriceProperty() {
+        return crystitePriceProperty;
+    }
+    
     
     public String toString() {
 		StringBuilder str = new StringBuilder();
 		str.append("The store stock is: \n");
-		str.append("food stock: " + foodStock + " at $" + foodPrice + " per unit\n");
-		str.append("energy stock: " + energyStock + " at $" + energyPrice + " per unit\n");
-		str.append("smithore stock: " + smithoreStock + " at $" + smithorePrice + " per unit\n");
-		str.append("crystite stock: " + crystiteStock + " at $" + crystitePrice + " per unit\n");
+		str.append("food stock: " + foodStockProperty.get() + " at $" + foodPriceProperty.get() + " per unit\n");
+		str.append("energy stock: " + energyStockProperty.get() + " at $" + energyPriceProperty.get() + " per unit\n");
+		str.append("smithore stock: " + smithoreStockProperty.get() + " at $" + smithorePriceProperty.get() + " per unit\n");
+		str.append("crystite stock: " + crystiteStockProperty.get() + " at $" + crystitePriceProperty.get() + " per unit\n");
     	
     	return str.toString();
     	
--- MULEgame/src/controller/GeneralStoreController.java
+++ MULEgame/src/controller/GeneralStoreController.java
@@ -5,11 +5,15 @@ import java.util.ResourceBundle;
 
 import application.Main;
 import application.Player;
+import application.Store;
 import application.Map.MapSelection;
+import javafx.beans.binding.Bindings;
 import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 
 public class GeneralStoreController implements Initializable, ControlledScreen, Loadable {
@@ -18,15 +22,58 @@ public class GeneralStoreController implements Initializable, ControlledScreen, 
 	
 	@Override
 	public void onLoad() {
+		Store store = Main.game.getStore();
 		String mapID = Main.game.getMap().getMapID();
 		MapController mapController;
 		MapSelection mapSelection = Main.game.getMap().getMapSelection();
 		
 		mapController = (MapController) myController.getController(mapID);
+		
 		ProgressBar mapBar = mapController.getTimerTask().getTimerBar();
 		DoubleProperty progProp = mapBar.progressProperty();
 		timerBarGeneralStore.progressProperty().bind(progProp);	
 
+		Player player = Main.game.getActivePlayer();
+		
+		SimpleIntegerProperty playerNumProperty = player.getPlayerIdProperty();
+		playerIdLBL.textProperty().bind(Bindings.convert(playerNumProperty));
+		
+		SimpleIntegerProperty playerMoneyProperty = player.getMoneyProperty();
+		playerMoneyLBL.textProperty().bind(Bindings.convert(playerMoneyProperty));
+		
+		SimpleIntegerProperty playerFoodProperty = player.getFoodProperty();
+		playerFoodLBL.textProperty().bind(Bindings.convert(playerFoodProperty));
+		
+		SimpleIntegerProperty playerEnergyProperty = player.getEnergyProperty();
+		playerEnergyLBL.textProperty().bind(Bindings.convert(playerEnergyProperty));
+		
+		SimpleIntegerProperty playerSmithoreProperty = player.getSmithoreProperty();
+		playerSmithoreLBL.textProperty().bind(Bindings.convert(playerSmithoreProperty));
+		
+		SimpleIntegerProperty storeFoodStockProperty = store.getFoodStockProperty();
+		storeFoodStockLBL.textProperty().bind(Bindings.convert(storeFoodStockProperty));
+		
+		SimpleIntegerProperty storeEnergyStockProperty = store.getEnergyStockProperty();
+		storeEnergyStockLBL.textProperty().bind(Bindings.convert(storeEnergyStockProperty));
+		
+		SimpleIntegerProperty storeSmithoreStockProperty = store.getSmithoreStockProperty();
+		storeSmithoreStockLBL.textProperty().bind(Bindings.convert(storeSmithoreStockProperty));
+	
+		SimpleIntegerProperty storeCrystiteStockProperty = store.getCrystiteStockProperty();
+		storeCrytiteStockLBL.textProperty().bind(Bindings.convert(storeCrystiteStockProperty));
+		
+		SimpleIntegerProperty storeFoodPriceProperty = store.getFoodPriceProperty();
+		storeFoodPriceLBL.textProperty().bind(Bindings.convert(storeFoodPriceProperty));
+		
+		SimpleIntegerProperty storeEnergyPriceProperty = store.getEnergyPriceProperty();
+		storeEnergyPriceLBL.textProperty().bind(Bindings.convert(storeEnergyPriceProperty));
+		
+		SimpleIntegerProperty storeSmithorePriceProperty = store.getSmithorePriceProperty();
+		storeSmithorePriceLBL.textProperty().bind(Bindings.convert(storeSmithorePriceProperty));
+		
+		SimpleIntegerProperty storeCrystitePriceProperty = store.getCrystitePriceProperty();
+		storeCrystitePriceLBL.textProperty().bind(Bindings.convert(storeCrystitePriceProperty));
+		
 	}
 
 	@Override
@@ -72,7 +119,44 @@ public class GeneralStoreController implements Initializable, ControlledScreen, 
 	@FXML 
 	private Button returnToTownBTN;
 	
+	@FXML
+	private Label playerIdLBL;
 	
+	@FXML
+	private Label playerMoneyLBL;
+	
+	@FXML
+	private Label playerFoodLBL;
+	
+	@FXML
+	private Label playerEnergyLBL;
+	
+	@FXML
+	private Label playerSmithoreLBL;
+	
+	@FXML
+	private Label storeFoodStockLBL;
+	
+	@FXML
+	private Label storeEnergyStockLBL;
+	
+	@FXML
+	private Label storeSmithoreStockLBL;
+	
+	@FXML
+	private Label storeCrytiteStockLBL;
+	
+	@FXML
+	private Label storeFoodPriceLBL;
+	
+	@FXML
+	private Label storeEnergyPriceLBL;
+	
+	@FXML
+	private Label storeSmithorePriceLBL;
+	
+	@FXML
+	private Label storeCrystitePriceLBL;
 	
 	
 	public void sellFoodClick() {
