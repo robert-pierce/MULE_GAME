4109df0e1632fa03108a431a41ac899af89982ff
Robert Pierce <rpierce31@gatech.edu>
2015-11-04 13:34:13 -0500

    Added a pop up for game save -Robert

--- MULEgame/src/application/GameSaver.java
+++ MULEgame/src/application/GameSaver.java
@@ -7,11 +7,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
 
 import jfx.messagebox.MessageBox;
 
@@ -21,47 +18,34 @@ public class GameSaver  {
 	
 	public void saveState() {
 		ArrayList<Player> plyrList = Main.game.getPlayerList();
-		String fileName = new SimpleDateFormat("yyyy-MM-dd-hh-mm'.ser'").format(new Date());
-		
 		
 		try {
-	         FileOutputStream fileOut = new FileOutputStream("./SavedStates/" + fileName);
+	         FileOutputStream fileOut = new FileOutputStream("./SavedStates/test.ser");
 	         ObjectOutputStream out = new ObjectOutputStream(fileOut);
 	         
-	         //write the map
-	         //out.writeObject(Main.game.getMap());
+	         //save 
 	         
-	         // write the store
-	         Main.game.getStore().prepSave();
-	         out.writeObject(Main.game.getStore());
-	         
-	         // write all of the players (in order)
 	         for (Player plyr : plyrList) {
 	        	  plyr.prepSave();
 	        	  out.writeObject(plyr);  
 	         }
 	         
-	         // write and EOF object as last element
 	         out.writeObject(new EOFClass());
-	         out.close();
-	         
-	         // terminate stream and close file
 	         out.close();
 	         fileOut.close();
 	         System.out.printf("Serialized data is saved");
-	         System.out.println("file name:" + fileName);
 	      
 	         MessageBox.show(Main.game.getScene().getWindow(),
-	        			"Game Saved Successfully",
-	      		         "Successful Game Save",
-	      		         MessageBox.ICON_INFORMATION | MessageBox.OK); 
+	    			 "Game Saved Successfuly",
+	  		         "Save Game",
+	  		         MessageBox.ICON_INFORMATION | MessageBox.OK);
 	         
 		} catch(IOException e) {
 	          e.printStackTrace();
 	      }
 		}
 	
-	public static class EOFClass implements Serializable { 
+	public static class EOFClass implements Serializable {
 		private static final long serialVersionUID = 4773733498081494931L;
 
 		public EOFClass() {};
